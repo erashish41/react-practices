750f30ac0dc35c5d07804a1448f713bfdebde145
ashish <erashish41@gmail.com>
2024-05-03 00:11:37 +0530

    update

--- react-pr/01vitereact/src/main.jsx
+++ react-pr/01vitereact/src/main.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+function MyAPP(){
+    return (
+        <div>
+            <h1>Custom App</h1>
+        </div>
+    )
+}
+
+const ReactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).
+render(
  
     <App />
  
