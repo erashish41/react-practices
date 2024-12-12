c9e1a41e23f1ab587909c1fcc76018cedd5346ae
Ashish Bhardwaj <erashish41@gmail.com>
2024-12-12 15:39:37 +0530

    Child to Parent child

--- react-pr/04ParentChild/src/componets/ChildComponent.jsx
+++ react-pr/04ParentChild/src/componets/ChildComponent.jsx
@@ -1,12 +1,28 @@
+// // Parent to Child passing data
+// import React from 'react'
+
+// const ChildComponent = ({parentToChild}) => {
+//   return (
+//     <div>
+//         <div>Child Component</div>
+//         <div>{parentToChild}</div>
+//     </div>
+    
+//   )
+// }
+
+// export default ChildComponent
+
+// Child to Parent passing data
 import React from 'react'
 
-const ChildComponent = ({parentToChild}) => {
+const ChildComponent = (props) => {
+    const data = "Data from Child to Parent"
   return (
     <div>
-        <div>Child Component</div>
-        <div>{parentToChild}</div>
+            <div>ChildComponent</div>
+          <button onClick={() =>props.handleProcess(2000)}>Click to Child</button>  
     </div>
-    
   )
 }
 
--- react-pr/04ParentChild/src/componets/ParentComponent.jsx
+++ react-pr/04ParentChild/src/componets/ParentComponent.jsx
@@ -1,21 +1,52 @@
+// // Parent to Child passing data
 import React, { useState } from 'react'
 import ChildComponent from './ChildComponent'
 
-const ParentComponent = () => {
+// const ParentComponent = () => {
     
-    const [data, setData] = useState('');
+//     const [data, setData] = useState('');
 
-    const parentToChild = () => {
-        setData("hello kki hal chal");
+//     const parentToChild = () => {
+//         setData("data from Parent to Child");
+//     }
+
+//   return (
+//     <div>
+//         <ChildComponent parentToChild={data}/>
+
+//         <div>
+//             <button onClick={() => parentToChild()}>Click on Parent</button>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default ParentComponent
+
+
+
+// Child to Parent passing data
+const ParentComponent = () => {    
+
+    const [info, setInfo] = useState('')
+  
+    const process = (data) => {
+        setInfo(data)
+        console.log(`hello data is: ${data}`);
+        
     }
 
   return (
     <div>
-        <ChildComponent parentToChild={data}/>
-
-        <div>
-            <button onClick={() => parentToChild()}>Click on Parent</button>
-        </div>
+            <div>ParentComponent</div>
+            <div>
+                {
+                    info ? <h2>data from child is {info}</h2> : <h2>no data is shown</h2>
+                }
+            </div>
+            <div>
+                <ChildComponent handleProcess={process}/>
+            </div>
     </div>
   )
 }
