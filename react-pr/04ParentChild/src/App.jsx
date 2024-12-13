99544188e2a96d31db9dbf05c90037721b71f1f1
Ashish Bhardwaj <erashish41@gmail.com>
2024-12-13 16:32:04 +0530

    props updates

--- react-pr/04ParentChild/src/App.jsx
+++ react-pr/04ParentChild/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ParentComponent from './componets/ParentChild/ParentComponent'
-import Profile from './componets/Profile'
+import Profile from './componets/Props/Profile'
 
 const App = () => {
   return (
