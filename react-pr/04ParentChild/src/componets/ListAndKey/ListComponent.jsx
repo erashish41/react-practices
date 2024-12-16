ce42722309e9d3a8a605389ae19af71a627fd0c3
Ashish Bhardwaj <erashish41@gmail.com>
2024-12-16 16:41:42 +0530

    updates

--- react-pr/04ParentChild/src/componets/ListAndKey/ListComponent.jsx
+++ react-pr/04ParentChild/src/componets/ListAndKey/ListComponent.jsx
@@ -6,9 +6,14 @@ const ListComponent = () => {
     const newList = ["100", "200", "300", "400", "500"]
 
     const [selectedItems, SetSelectedItems] = useState(null)
+    // console.log(selectedItems);
+        
 
-    const handleItemSelect = (data) => {
-        SetSelectedItems(data)
+    const handleItemSelect = (infoReceived) => {
+        // infoReceived is passed as props
+        SetSelectedItems(infoReceived)
+        // console.log('data infoReceived is : ',infoReceived);
+        
     }
 
   return (
@@ -17,15 +22,15 @@ const ListComponent = () => {
         <div>
             <ul>
                 {
-                    newList.map((items, index) => (
-                        <li
-                            key={index}
-                            onClick={() =>handleItemSelect(items)}
-                            style={{cursor: 'pointer'}}
-                        >
+                    newList.map((items, index) => {
+                        {/* console.log("items :", items,", index :", index) */}
+                        return (
+                        <li key={index} onClick={() =>handleItemSelect(items)} style={{cursor: 'pointer'}}>
+                            {/* items are passed as argument */}
+                            {console.log("hi..", items )}
                             {items}
-                        </li>
-                    ))
+                        </li>)
+                })
                 }
             </ul>
         </div>
@@ -35,3 +40,5 @@ const ListComponent = () => {
 }
 
 export default ListComponent
+
+// items and index in map method are passed as argument and passed automatically by map method.
--- react-pr/04ParentChild/src/componets/ListAndKey/ListDetail.jsx
+++ react-pr/04ParentChild/src/componets/ListAndKey/ListDetail.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
 const ListDetail = ({passItems}) => {
+  // console.log("received items..", passItems);
+  
+  
   return (
     <div>
         <div>List Detail</div>
         {
             passItems ? <p>you passed items: {passItems}</p> : <p>Please pass the items from the list</p>
+            
         }
+        {/* {console.log("heeloo...", passItems)} */}
     </div>
   )
 }
